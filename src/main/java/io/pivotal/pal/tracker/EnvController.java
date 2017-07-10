68a3bc869cd1da1137f4811692d00d461b66c99a
Conor Nosal <cnosal@pivotal.io>
2017-07-10 15:44:08 -0400

    Extract welcome message to environment variable

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,7 @@
+package io.pivotal.pal.tracker;
+
+/**
+ * Created by pivotal on 7/10/17.
+ */
+public class EnvController {
+}
--- src/main/java/io/pivotal/pal/tracker/TrackerController.java
+++ src/main/java/io/pivotal/pal/tracker/TrackerController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TrackerController {
+
+    @Value("${welcome_message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
