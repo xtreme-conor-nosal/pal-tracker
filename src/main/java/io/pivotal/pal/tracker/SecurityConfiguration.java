750700c9214619050995422e4ec22407dfc32d62
Conor Nosal <cnosal@pivotal.io>
2017-07-12 09:58:16 -0400

    Fix security env var

--- src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
+++ src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
@@ -3,16 +3,11 @@ package io.pivotal.pal.tracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * Created by pivotal on 7/12/17.
- */
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         String forceHttps = System.getenv("SECURITY_FORCE_HTTPS");
--- src/test/java/io/pivotal/pal/trackertest/PalTrackerTest.java
+++ src/test/java/io/pivotal/pal/trackertest/PalTrackerTest.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 import java.util.Collection;
