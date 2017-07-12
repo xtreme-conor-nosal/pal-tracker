8a0aff6daa94428b6e06ec6a07fe4372ec19696d
Conor Nosal <cnosal@pivotal.io>
2017-07-12 09:35:33 -0400

    Add security configuration

--- src/test/java/io/pivotal/pal/trackertest/PalTrackerTest.java
+++ src/test/java/io/pivotal/pal/trackertest/PalTrackerTest.java
@@ -9,9 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -29,12 +31,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PalTrackerTest {
 
-    @Autowired
     private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private String port;
 
     @Before
     public void setUp() throws Exception {
         DataSource dataSource = new MariaDbDataSource(System.getenv("SPRING_DATASOURCE_URL"));
+
+        RestTemplateBuilder builder = new RestTemplateBuilder()
+                .rootUri("http://localhost:" + port)
+                .basicAuthorization("user", "password");
+
+        restTemplate = new TestRestTemplate(builder);
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         jdbcTemplate.execute("TRUNCATE time_entries");
