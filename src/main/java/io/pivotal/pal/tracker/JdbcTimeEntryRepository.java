4f81998eebddc27381580a122a52a91069411852
Conor Nosal <cnosal@pivotal.io>
2017-07-12 09:40:58 -0400

    Fix jdbc query

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -1,6 +1,7 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 
@@ -39,7 +40,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
     @Override
     public TimeEntry get(Long id) {
-            return jdbcTemplate.queryForObject(
+            return jdbcTemplate.query(
                 "SELECT id, project_id, user_id, date, hours FROM time_entries WHERE id = ?",
                 new Object[]{id},
                 extractor);
